aca5725ab1fb9fb1abae5d5e1c619cb7edec513c
Alejandra Escobar <aleja.escobar2501@gmail.com>
2024-10-17 11:22:30 -0500

    metodo es valida2

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -44,6 +44,12 @@ class Validador:
     def __init__(self, regla: ReglaValidacion):
         self.regla = regla
 
+    def es_valida(self, clave: str) -> bool:
+        if self.regla.es_valida(clave):
+            return True
+        else:
+            return False
+
 
 class ReglaValidacionGanimedes(ReglaValidacion):
     pass
