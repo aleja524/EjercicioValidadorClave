88a4ea40955361462a499d0d0ce39c7805a0fb9a
Alejandra Escobar <aleja.escobar2501@gmail.com>
2024-10-17 11:02:08 -0500

    metodo es valida

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,25 @@
 # TODO: Implementa el código del ejercicio aquí
+
+from abc import ABC, abstractmethod
+from validadorclave.modelo.errores import *
+
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, longitud_esperada: int):
+        self._longitud_esperada: int = longitud_esperada
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+
+
+
+
+
+
+
+
+
